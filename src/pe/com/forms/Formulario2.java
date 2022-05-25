00bbae62db7bc55663ce1e784f5e13758c63a763
Jhan Frank <JhnFrankz@gmail.com>
2022-05-25 10:03:05 -0500

    Avance Formulario para escoger moneda a convertir

--- src/pe/com/forms/Formulario2.java
+++ src/pe/com/forms/Formulario2.java
@@ -9,17 +9,18 @@ public class Formulario2 extends JFrame implements ActionListener {
     private JTextField textField1;
     private JButton button1;
     private JButton button2;
-    private String valorIngresado;
+    private double valorIngresado;
 
     public Formulario2() {       //width: 300    height: 200
         this.setTitle("Input");
         setLayout(null);
-        this.label1 = new JLabel("Seleccione una opción de conversión: ");
-        this.label1.setBounds(10, 10, 280, 30);
+        this.label1 = new JLabel("Ingresa la cantidad de dinero que" +
+                " deseas convertir: ");
+        this.label1.setBounds(10, 10, 380, 30);
         add(this.label1);
 
         this.textField1 = new JTextField();
-        this.textField1.setBounds(10, 50, 260, 30);
+        this.textField1.setBounds(10, 50, 340, 30);
         add(this.textField1);
 
         this.button1 = new JButton("Aceptar");
@@ -27,7 +28,7 @@ public class Formulario2 extends JFrame implements ActionListener {
         add(this.button1);
         this.button1.addActionListener(this);   //Indicamos que el evento es en este boton
 
-        this.button2 = new JButton("Cancelar");
+        this.button2 = new JButton("Cerrar");
         this.button2.setBounds(120, 100, 90, 30);
         add(this.button2);
         this.button2.addActionListener(this);
@@ -37,8 +38,35 @@ public class Formulario2 extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == this.button1) {
-            this.valorIngresado = this.textField1.getText();
-            System.out.println(this.valorIngresado);
+            String valor;
+            valor = this.textField1.getText();
+            if (valor.isBlank()) {
+                //El input nunca será null, si está vacio manda ""
+                JOptionPane.showMessageDialog(null,
+                        "El campo no puede estar vacio.");
+                this.textField1.setText("");
+            } else {
+                //Si el input no está en blanco, se valida si es un numero
+                boolean esNumero = false;
+
+                try {
+                    this.valorIngresado = Double.parseDouble(valor);
+                    esNumero = true;
+                    System.out.println(this.valorIngresado);
+                } catch (NumberFormatException numberFormatException) {
+                    JOptionPane.showMessageDialog(null,
+                            "Solo puede ingresar un número.");
+                }
+
+                if (esNumero) {
+                    Formulario3 formulario3 = new Formulario3();
+                    formulario3.setBounds(0, 0, 400, 200);
+                    formulario3.setVisible(true);
+                    formulario3.setLocationRelativeTo(null);
+                    formulario3.setResizable(false);
+                    this.setVisible(false);
+                }
+            }
             //Dar validación, mostrar showInput encaso no sea numero
         } else if (e.getSource() == this.button2) {
             System.exit(0);
--- src/pe/com/forms/Formulario3.java
+++ src/pe/com/forms/Formulario3.java
@@ -6,28 +6,37 @@ import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
-public class Formulario1 extends JFrame implements ActionListener, ItemListener {
+public class Formulario3 extends JFrame implements ActionListener, ItemListener {
     private JLabel label1;
     private JComboBox<String> comboBox1;
     private JButton button1;
     private JButton button2;
     private int conversorSeleccionado = 0;
 
-    public Formulario1() {       //width: 300    height: 200
+    public Formulario3() {       //width: 300    height: 200
         this.setTitle("Menu");
         setLayout(null);
-        this.label1 = new JLabel("Seleccione una opción de conversión: ");
-        this.label1.setBounds(10, 10, 2380, 30);
+        this.label1 = new JLabel("Elije la moneda a la que deseas" +
+                "convertir tu dinero:: ");
+        this.label1.setBounds(10, 10, 280, 30);
         add(this.label1);
 
         this.comboBox1 = new JComboBox<>();
-        this.comboBox1.setBounds(10, 50, 360, 30);
+        this.comboBox1.setBounds(10, 50, 260, 30);
         add(this.comboBox1);
-        this.comboBox1.addItem("Conversor de monedas");
-        this.comboBox1.addItem("Conversor de temperaturas");
+        this.comboBox1.addItem("De Soles a Dólar");
+        this.comboBox1.addItem("De Soles a Euro");
+        this.comboBox1.addItem("De Soles a Libras");
+        this.comboBox1.addItem("De Soles a Yen");
+        this.comboBox1.addItem("De Soles a Won Coreano");
+        this.comboBox1.addItem("De Dólar a Soles");
+        this.comboBox1.addItem("De Euro a Soles");
+        this.comboBox1.addItem("De Libras a Soles");
+        this.comboBox1.addItem("De Yen a Soles");
+        this.comboBox1.addItem("De Won Coreano a Soles");
         this.comboBox1.addItemListener(this);
 
-        this.button1 = new JButton("Aceptar");
+        this.button1 = new JButton("Convertir");
         this.button1.setBounds(10, 100, 90, 30);
         add(this.button1);
         this.button1.addActionListener(this);   //Indicamos que el evento es en este boton
@@ -42,17 +51,17 @@ public class Formulario1 extends JFrame implements ActionListener, ItemListener 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == this.button1) {
-            if (this.conversorSeleccionado == 0) {
-                System.out.println("Ir al conversor de monedas");
+            System.out.println("Ir a las monedas convertidas");
+            /*if (this.conversorSeleccionado == 0) {
                 Formulario2 formulario2 = new Formulario2();
-                formulario2.setBounds(0, 0, 400, 200);
+                formulario2.setBounds(0, 0, 300, 200);
                 formulario2.setVisible(true);
                 formulario2.setLocationRelativeTo(null);
                 formulario2.setResizable(false);
                 this.setVisible(false);
             } else if (this.conversorSeleccionado == 1) {
                 System.out.println("Ir al conversor de temperaturas");
-            }
+            }*/
         } else if (e.getSource() == this.button2) {
             System.exit(0);
         }
